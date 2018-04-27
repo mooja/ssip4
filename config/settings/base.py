3a81d450ade2adbf0e23bf79b026fb40c63e4176
Mooja <max.atreides@gmail.com>
2018-04-27 19:10:51 -0400

    added news model

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,7 +70,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'ssip4.users.apps.UsersConfig',
-    # Your stuff: custom apps go here
+    'news'
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ ssip4/news/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from model_utils import Choices
+from model_utils.fields import StatusField
+from model_utils.models import TimeStampedModel
+from model_utils.managers import QueryManager
+
+# Create your models here.
+class NewsEntry(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True)
+    author = models.CharField(max_length=200, null=True, blank=True)
+
+    pub_date = models.DateField(null=False)
+
+    NEWSCHOICES = Choices('news', 'membernews')
+    newstype = StatusField(choices_name='NEWSCHOICES')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "News Entries"
