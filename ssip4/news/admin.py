f8fb61fdd807cf27cf285e2f5afe9d07a7deb647
Mooja <max.atreides@gmail.com>
2018-04-27 19:53:53 -0400

    news admin fields

--- ssip4/news/admin.py
+++ ssip4/news/admin.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.contrib import admin
+# from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
-# Register your models here.
+from .models import NewsEntry
+
+
+class NewsEntryAdminForm(forms.ModelForm):
+    # text = forms.CharField(widget=SummernoteWidget)
+    text = forms.CharField()
+
+    class Meta:
+        model = NewsEntry
+        exclude = ['created']
+
+
+class NewsEntryAdmin(admin.ModelAdmin):
+    form = NewsEntryAdminForm
+    list_display = ['title', 'created']
+
+admin.site.register(NewsEntry, NewsEntryAdmin)
